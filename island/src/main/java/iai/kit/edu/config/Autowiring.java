9068b002c49a011528e473bdecf08848984cab66
ueewu <ueewu@kit.edu>
2019-11-17 12:37:49 +0100

    run migration synchronization service locally

--- island/src/main/java/iai/kit/edu/config/Autowiring.java
+++ island/src/main/java/iai/kit/edu/config/Autowiring.java
@@ -30,7 +30,8 @@ public class Autowiring {
     @Bean
     JedisConnectionFactory jedisConnectionFactory() {
         JedisConnectionFactory jedisConFactory = new JedisConnectionFactory();
-        jedisConFactory.setHostName("redis");
+        //jedisConFactory.setHostName("redis");
+        jedisConFactory.setHostName("localhost");
         jedisConFactory.setPort(6379);
         return jedisConFactory;
     }
