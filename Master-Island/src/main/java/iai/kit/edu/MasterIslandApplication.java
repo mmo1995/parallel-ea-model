1ec34d409756c7625d87c81184a10964fec5b3be
ueewu <ueewu@kit.edu>
2019-11-28 15:20:16 +0100

    add new service "Master-Island"

---
+++ Master-Island/src/main/java/iai/kit/edu/MasterIslandApplication.java
@@ -0,0 +1,19 @@
+package iai.kit.edu;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
+
+@SpringBootApplication
+public class MasterIslandApplication{
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(MasterIslandApplication.class, args);
+
+    }
+
+
+}
