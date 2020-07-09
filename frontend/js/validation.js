5864dcc48de5484ab59283f969f4c1f3e7d933cd
ueewu <ueewu@kit.edu>
2020-07-09 19:31:42 +0200

    finish implementing validation rules

--- frontend/js/validation.js
+++ frontend/js/validation.js
@@ -3,6 +3,9 @@ var topologyValidValues = ["ring", "biRing", "ladder", "complete"];
 var selectionPolicyValidValues = ["best", "random"];
 var replcaementPolicyValidValues = ["worst","random"];
 var asyncMigrationValidValues = ["true","false"];
+var acceptOffspringValidValues = ["always","localLeast","localLeast-ES", "betterParent"];
+var globalCriterionValidValues = ["fitness","evaluation","generation"];
+var epochCriterionValidValues = ["fitness", "evaluation", "generation"];
 
 function checkValidation(){
     var inputIslandsNumber = $("#islands-number").val();
@@ -18,6 +21,13 @@ function checkValidation(){
     var inputReplacementPolicy = $("#replacement-policy").val();
     var inputDemeSize = $("#deme-size").val();
     var inputAsyncMigration = $("#async-migration").val();
+    var inputAcceptOffspring = $("#accept-offspring").val();
+    var inputRankingParameter = $("#ranking-parameter").val();
+    var inputMinHamming = $("#min-hamming").val();
+    var inputGlobalCriterion = $("#global-criterion").val();
+    var inputGlobalLimit = $("#global-criterion-limit").val();
+    var inputEpochCriterion = $("#epoch-criterion").val();
+    var inputEpochLimit = $("#epoch-criterion-limit").val();
 
     if($("select#models").val()=="island" || $("select#models").val()=="hybrid"){
         if(parseInt(inputIslandsNumber)>5 || parseInt(inputIslandsNumber)<1 || inputIslandsNumber.length == 0){
@@ -36,6 +46,12 @@ function checkValidation(){
             return false;
         }
         if(asyncMigrationValidValues.indexOf(inputAsyncMigration)==-1){
+            return false;
+        }
+        if(epochCriterionValidValues.indexOf(inputEpochCriterion)==-1){
+            return false;
+        }
+        if(inputEpochLimit.length == 0){
             return false;
         }
 
@@ -64,8 +80,21 @@ function checkValidation(){
     if(parseInt(inputDemeSize)>8 || parseInt(inputDemeSize)<1 || inputDemeSize.length == 0){
         return false;
     }
-
-    
+    if(acceptOffspringValidValues.indexOf(inputAcceptOffspring)==-1){
+        return false;
+    }
+    if(parseFloat(inputRankingParameter)>2 || parseFloat(inputRankingParameter)<1 || inputRankingParameter.length == 0){
+        return false;
+    }
+    if(parseFloat(inputMinHamming)>1 || parseFloat(inputMinHamming)<0 || inputMinHamming.length == 0){
+        return false;
+    }
+    if(globalCriterionValidValues.indexOf(inputGlobalCriterion)==-1){
+        return false;
+    }
+    if(inputGlobalLimit.length == 0){
+        return false;
+    }
 
     return true;
 }
