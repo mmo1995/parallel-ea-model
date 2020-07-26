a79d8dcd25ef5d4edee11ac4a610c1290e298ab0
Mohammad Mohammad <kermalak_10@hotmail.com>
2020-07-26 23:35:35 +0200

    delete unused folders

--- opal-simulator/commands.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-python gleam-simulator-read-from-redis.py & python gleam-simulator-write-to-redis.py & ./testOpalSimu_2 127.0.0.1 $1 $2
--- testContainer/redisTest.py
+++
@@ -1,7 +0,0 @@
-import redis
-import os
-r = redis.StrictRedis()
-test = r.incr("test")
-c = 1
-if c != 0:
-    print "LAJBS"
